f092f81f5a73f41496077747676d16e1089985d9
VitasVital <48451734+VitasVital@users.noreply.github.com>
2019-10-18 12:08:48 +0300

    AmirTerrorist

---
+++ AmirTerrorist/AmirTerrorist/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmirTerrorist
+{
+    public partial class Form1 : Form
+    {
+        Graphics gr;
+        Terrorist terrorist = new Terrorist();
+        Image back;
+        int delta = 0;
+        Bullet bullet = new Bullet();
+        public Form1()
+        {
+            InitializeComponent();
+            back = Image.FromFile(Directory.GetCurrentDirectory() + @"\dedust.jpg");
+            gr = panel1.CreateGraphics();
+            bullet.position.X = -100;
+            bullet.position.Y = -100;
+            //typeof(Panel).InvokeMember("DoubleBuffered",
+            //BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+            //null, panel1, new object[] { true });
+        }
+        //private void panel1_Paint(object sender, PaintEventArgs e)
+        //{
+        //    e.Graphics.DrawImage(terrorist.terrorist, terrorist.position);
+        //    e.Graphics.DrawImage(back, 0, 0);
+        //    e.Graphics.DrawImage(bullet.bullet, bullet.position);
+        //}
+
+        private void shoot()
+        {
+            bullet.position = terrorist.position;
+            while (bullet.position.Y > 0)
+            {
+                bullet.position.Y = -1;
+                Thread.Sleep(100);
+            }
+            bullet.position.Y = -100;
+            bullet.position.X = -100;
+        }
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Thread shooting = new Thread(new ThreadStart(shoot));
+            shooting.Start();
+        }
+        public class Bullet
+        {
+            public Point position;
+            public Image bullet;
+            public Bullet()
+            {
+                position.X = 0;
+                position.Y = 0;
+                bullet = Image.FromFile(Directory.GetCurrentDirectory() + @"\bullet.jpg");
+            }
+        }
+        public class Terrorist
+        {
+            public Point position;
+            public Image terrorist;
+            public Terrorist()
+            {
+                position.X = 300;
+                position.Y = 300;
+                terrorist = Image.FromFile(Directory.GetCurrentDirectory() + @"\terrorist.png");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //delta += timer1.Interval;
+            //if (delta > 20)
+            //{
+            //    panel1.Refresh();
+            //    delta = 0;
+            //}
+            gr.DrawImage(terrorist.terrorist, terrorist.position);
+            gr.DrawImage(back, 0, 0);
+            gr.DrawImage(bullet.bullet, bullet.position);
+        }
+    }
+}
---
+++ AmirTerrorist/AmirTerrorist/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("AmirTerrorist")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AmirTerrorist")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1a25123b-da44-4f3b-8680-c61f0b33556b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
